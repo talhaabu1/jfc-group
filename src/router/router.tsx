5c3f22b08b43f55f9d6f1cfbd8a9d927f72d246f
Abu Talha <108407737+talhaabu1@users.noreply.github.com>
2024-10-13 17:45:02 +0600

    port

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,7 +4,7 @@ import HomePage from "@/page/HomePage";
 import AddCustomer from "@/page/Customer/AddCustomer";
 import AddDriver from "@/page/Driver/AddDriver";
 import AddHelper from "@/page/Helper/AddHelper";
-import CreateBilling from "@/page/Tractor/createBilling";
+import CreateBilling from "@/page/Tractor/CreateBilling";
 
 const router = createBrowserRouter([
   {
